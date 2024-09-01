71def96041d95281afab652a42d939f33699040e
Sivamani <106467109+5maurosivamani@users.noreply.github.com>
2024-09-01 09:51:15 +0530

    Display all the store's use geoJSON objects use leaflet

--- geoLocationAPI.js
+++ geoLocationAPI.js
@@ -1,7 +1,7 @@
 /**
  * Geo Location API
  */
-
+/*
 // navigator.geolocation object will provide the user location
 if (navigator.geolocation) {
   console.log("Your browser supports the Geolocation API!");
@@ -56,7 +56,7 @@ function success(position) {
     title: "My Location",
     clickable: true,
     draggable: true,
-    icon: customIcon
+    icon: customIcon,
   };
 
   // create the marker
@@ -67,7 +67,7 @@ function success(position) {
 
   // bind popup
   marker.bindPopup("Your current location");
-}
+} */
 
 /**
  * Error code
@@ -91,3 +91,55 @@ function error(error) {
       break;
   }
 }
+
+/**
+ * GeoJSON and leaflet
+ */
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(success, error);
+}
+
+function success(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+
+  const mapOptions = {
+    center: [lat, lon],
+    zoom: 15,
+  };
+
+  const map = L.map("map", mapOptions);
+
+  const layer = L.tileLayer(
+    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  );
+
+  map.addLayer(layer);
+}
+
+function generateStoreList() {
+  const ul = document.getElementById("store-ul-list");
+
+  storeList.forEach((store) => {
+    const li = document.createElement("li");
+    const div = document.createElement("div");
+    const a = document.createElement("a");
+    const p = document.createElement("p");
+
+    // li.classList.add("border-b","border-gray-200", "last:border-b-0");
+    div.classList.add("mt-3");
+    a.classList.add("text-green-500", "text-2xl", "font-semibold");
+    p.classList.add("text-gray-200", "text-base");
+
+    a.href = "#";
+    a.innerText = store.properties.name;
+    p.innerText = store.properties.address;
+
+    div.appendChild(a);
+    div.appendChild(p);
+    li.appendChild(div);
+    ul.appendChild(li);
+  });
+}
+
+generateStoreList();
---
+++ store.js
@@ -0,0 +1,35 @@
+const storeList = [
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [10.4620187, 79.8163561],
+    },
+    properties: {
+      name: "Senthur Murugan Stores",
+      address: "No.2 Sembotai, Nagapattinam, Tamil Nadu 611001",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [10.4620187,79.8163561],
+    },
+    properties: {
+      name: "Malar Maligai",
+      address: "FR6V+637, Pushpavanam, Tamil Nadu 614809",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [10.4750558,79.8093772],
+    },
+    properties: {
+      name: "Saravanan store",
+      address: "FRGH+2XH, Pushpavanam, Tamil Nadu 614809",
+    },
+  },
+];
